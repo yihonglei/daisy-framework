745937087592c643e538c7f3fdcec48969a41ed3
yihonglei <yihonglei@hqzhuanche.com>
2019-11-16 15:49:24 +0800

    common commit

---
+++ rose/rose-common/src/main/java/com/lanhuigu/common/cache/CacheManager.java
@@ -0,0 +1,8 @@
+package com.lanhuigu.common.cache;
+
+/**
+ * @author yihonglei
+ * @date 2019/11/16 3:48 PM
+ */
+public class CacheManager {
+}
---
+++ rose/rose-core/src/main/java/com/lanhuigu/core/interceptor/InterceptorTest.java
@@ -0,0 +1,8 @@
+package com.lanhuigu.core.interceptor;
+
+/**
+ * @author yihonglei
+ * @date 2019/11/16 3:48 PM
+ */
+public class InterceptorTest {
+}
