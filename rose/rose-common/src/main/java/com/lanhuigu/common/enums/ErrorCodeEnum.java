e1af2d6a8d4238325444623d6919f570c8112ba2
yihonglei <yihonglei@hqzhuanche.com>
2019-11-16 16:46:13 +0800

    错误码

--- rose/rose-common/src/main/java/com/lanhuigu/common/enums/ErrorCodeEnum.java
+++ rose/rose-common/src/main/java/com/lanhuigu/common/enums/ErrorCodeEnum.java
@@ -9,7 +9,6 @@ import lombok.Getter;
  * @date 2019/11/16 1:18 PM
  */
 public enum ErrorCodeEnum {
-    SUCCESS(1, "success"),
     SYSTEM_DEFAULT_ERROR(0, "网络异常,请您重试!"),
     ILLEGAL_ARGUMENT_ERROR(10000, "无效参数");
 
