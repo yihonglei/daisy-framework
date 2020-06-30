be5a2ef877361890dc86cad61094b45a891184ba
yihonglei <yihonglei@hqzhuanche.com>
2020-06-30 20:57:41 +0800

    注入Maps

---
+++ daisy/daisy-core/src/main/java/com/jpeony/core/service/beanmaps/AMapsBeanServiceImpl.java
@@ -0,0 +1,17 @@
+package com.jpeony.core.service.beanmaps;
+
+import com.jpeony.core.service.MapsBeanService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author yihonglei
+ */
+@Slf4j
+@Service("aMapsBeanService")
+public class AMapsBeanServiceImpl implements MapsBeanService {
+    @Override
+    public void sayHello(String name) {
+        log.info("sayHelloA={}", name);
+    }
+}
