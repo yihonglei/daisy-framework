1fb8c594c039f914a44094a8ff0493a10706b5e0
yihonglei <yihonglei@hqzhuanche.com>
2020-08-30 19:06:54 +0800

    代码优化

--- daisy-rocketmq-spring-boot-starter/src/main/java/com/jpeony/rocketmq/spring/lifecycle/AbstractLifeCycle.java
+++ daisy-rocketmq-spring-boot-starter/src/main/java/com/jpeony/rocketmq/spring/lifecycle/AbstractLifeCycle.java
@@ -1,20 +1,9 @@
 package com.jpeony.rocketmq.spring.lifecycle;
 
-import org.springframework.context.EnvironmentAware;
-import org.springframework.core.env.ConfigurableEnvironment;
-
 /**
  * @author yihonglei
  */
 public abstract class AbstractLifeCycle implements LifeCycle {
-
-    /**
-     * 从spring配置中读取value值
-     */
-    protected String getValue(ConfigurableEnvironment env, String key) {
-        return env.resolvePlaceholders(key);
-    }
-
     /**
      * 判断字符串是否为空
      */
