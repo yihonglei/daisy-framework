25200beb3592a9942e2827911a47f47178629d72
yihonglei <yihonglei@hqzhuanche.com>
2020-11-18 15:18:15 +0800

    swagger

--- daisy/daisy-api/src/main/java/com/jpeony/api/controller/SwaggerController.java
+++ daisy/daisy-api/src/main/java/com/jpeony/api/controller/SwaggerController.java
@@ -1,7 +1,7 @@
 package com.jpeony.api.controller;
 
-import com.jpeony.core.pojo.dto.TestDTO;
 import com.jpeony.common.utils.ApiResponse;
+import com.jpeony.core.pojo.dto.TestDTO;
 import com.jpeony.core.service.TestService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -12,29 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 控制层，service，请求规范，响应规范测试
+ * Swagger测试
  *
  * @author yihonglei
  */
 @Validated
 @RestController
-@RequestMapping("/test")
-public class TestController {
+@RequestMapping("swagger")
+@Api(value = "SwaggerController", tags = {"Swagger相关"})
+public class SwaggerController {
     @Autowired
     private TestService testService;
 
-    @RequestMapping("/test")
-    public ApiResponse test() {
-        return ApiResponse.success("daisy启动成功了!");
-    }
-
+    @ApiOperation(value = "查询用户信息", notes = "用户查询", response = ApiResponse.class)
     @PostMapping("/queryTestById")
     public ApiResponse queryTestById(TestDTO testDTO) {
         return ApiResponse.success(testService.queryTestById(testDTO));
-    }
-
-    @RequestMapping("queryTestByIdXml")
-    public ApiResponse queryTestByIdXml(TestDTO testDTO) {
-        return ApiResponse.success(testService.queryTestByIdXml(testDTO));
     }
 }
