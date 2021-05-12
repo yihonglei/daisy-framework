966522b6e9d11b2177d54f423113a0641996a581
yihonglei <yihonglei@hqzhuanche.com>
2021-05-12 21:32:23 +0800

    nacos

---
+++ daisy-springcloud-framework/daisy-cloud-nacos/daisy-cloud-user/src/main/java/com/jpeony/cloud/user/UserApplication.java
@@ -0,0 +1,16 @@
+package com.jpeony.cloud.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author yihonglei
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
---
+++ daisy-springcloud-framework/daisy-cloud-nacos/daisy-cloud-user/src/main/java/com/jpeony/cloud/user/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.jpeony.cloud.user.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yihonglei
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Value("${user.name}")
+    private String userName;
+
+    @GetMapping("/helloUser")
+    public String helloUser() {
+        return "hello user, user.name = " + userName;
+    }
+}
