ebf82c687af3e7268f24e82e76d77e0abceb9f00
yihonglei <yihonglei@hqzhuanche.com>
2021-05-21 17:16:08 +0800

    test

--- daisy-springcloud-framework/daisy-cloud-nacos/daisy-cloud-auth/src/main/java/com/jpeony/cloud/App.java
+++ daisy-springcloud-framework/daisy-cloud-nacos/daisy-cloud-auth/src/main/java/com/jpeony/cloud/App.java
@@ -1,13 +1,7 @@
 package com.jpeony.cloud;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
--- daisy-springcloud-framework/daisy-cloud-nacos/daisy-cloud-order/src/test/java/com/jpeony/cloud/AppTest.java
+++ daisy-springcloud-framework/daisy-cloud-nacos/daisy-cloud-order/src/test/java/com/jpeony/cloud/AppTest.java
@@ -4,17 +4,9 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 }
