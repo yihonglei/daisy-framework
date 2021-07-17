e14a6101dd2572e8cd9aaa6f9a54afbed4475d7f
yihonglei <yihonglei@hqzhuanche.com>
2021-07-17 22:46:35 +0800

    gateway

--- daisy-springcloud-framework/daisy-cloud-user/src/main/java/com/jpeony/cloud/user/controller/UserController.java
+++ daisy-springcloud-framework/daisy-cloud-user/src/main/java/com/jpeony/cloud/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.jpeony.cloud.user.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @Value("${user.name:test}")
-    private String userName;
 
-    @GetMapping("/helloUser")
-    public String helloUser() {
-        return "hello user, user.name = " + userName;
+    @GetMapping("/getUser/{userId}")
+    public String helloUser(@PathVariable int userId) {
+        return "hello user, userName = test, userId = " + userId;
     }
 }
