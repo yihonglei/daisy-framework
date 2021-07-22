a1dc7af46fad48d71ccf4a02103abdcdb1331bbc
yihonglei <yihonglei@hqzhuanche.com>
2021-07-22 21:17:28 +0800

    微服务架构

--- daisy-dubbo-framework/daisy-dubbo-order/daisy-dubbo-order-client-api/src/test/java/com/jpeony/dubbo/AppTest.java
+++ daisy-dubbo-framework/daisy-dubbo-order/daisy-dubbo-order-client-api/src/test/java/com/jpeony/dubbo/AppTest.java
@@ -1,20 +1,7 @@
 package com.jpeony.dubbo;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
 }
