624ced0660897559e0dcf190416fb26b3d06d939
yihonglei <yihonglei@hqzhuanche.com>
2021-07-26 19:59:53 +0800

    nacos register

--- daisy-springcloud-framework/daisy-cloud-user/src/main/java/com/jpeony/cloud/user/controller/UserController.java
+++ daisy-springcloud-framework/daisy-cloud-user/src/main/java/com/jpeony/cloud/user/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.jpeony.cloud.user.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
