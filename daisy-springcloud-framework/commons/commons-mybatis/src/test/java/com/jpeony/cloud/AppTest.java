788786c134229f26191f73805e27ec432f1daff4
yihonglei <jpeony@163.com>
2021-08-19 20:59:59 +0800

    rename

--- daisy-springcloud-framework/commons/commons-mybatis/src/test/java/com/jpeony/cloud/AppTest.java
+++ daisy-springcloud-framework/commons/commons-mybatis/src/test/java/com/jpeony/cloud/AppTest.java
@@ -1,20 +1,7 @@
 package com.jpeony.cloud;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
 }
