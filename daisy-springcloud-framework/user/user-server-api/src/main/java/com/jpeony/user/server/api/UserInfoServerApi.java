eca09543457ee2562a32fb11075869f39c4c8268
yihonglei <jpeony@163.com>
2022-11-11 17:44:46 +0800

    sharding-jdbc 读写分离

---
+++ daisy-springcloud-framework/user/user-server-api/src/main/java/com/jpeony/user/server/api/UserInfoServerApi.java
@@ -0,0 +1,17 @@
+package com.jpeony.user.server.api;
+
+import com.jpeony.user.server.api.dto.UserInfoDTO;
+import com.jpeony.user.server.api.vo.UserInfoVO;
+import com.jpeony.commons.core.util.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * @author yihonglei
+ */
+@FeignClient(name = "user-server")
+public interface UserInfoServerApi {
+    @PostMapping(value = "/user/getUserInfo")
+    R<UserInfoVO> getUserInfo(@RequestBody UserInfoDTO userInfoDTO);
+}
---
+++ daisy-springcloud-framework/user/user-server/src/main/java/com/jpeony/user/server/controller/ConfigTestController.java
@@ -0,0 +1,25 @@
+package com.jpeony.user.server.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yihonglei
+ */
+@RequestMapping("config/test")
+@RestController
+@Slf4j
+public class ConfigTestController {
+
+    @Value("${test.name:ok}")
+    private String testName;
+
+    @GetMapping("/test")
+    public String test() {
+        log.info("testName={}", testName);
+        return "SUCCESS";
+    }
+}
